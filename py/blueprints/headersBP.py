dba72c862cbd3076c8593e67c6d79886bfed0936
UNGARO Cosimo <cosimo.ungaro@telecomnancy.eu>
2023-10-17 20:21:25 +0200

    Suppresion de redirections inutiles

--- py/blueprints/headersBP.py
+++ py/blueprints/headersBP.py
@@ -7,15 +7,3 @@ headersBP = Blueprint("headersBP", __name__)
 @headersBP.route('/headers', methods=['GET','POST'])
 def display():
     return render_template("headerstest.html")
-
-@headersBP.route("/bootstrap-css.css")
-def bootstrap_css():
-    return url_for('static',"css/bootstrap-css.css")
-
-@headersBP.route("/headers.css")
-def headers_css():
-    return url_for('static',"css/headers.css")
-
-@headersBP.route("/bootstrap.min.css")
-def boostrapmincss():
-    return url_for('static',"css/bootstrap.min.css")
